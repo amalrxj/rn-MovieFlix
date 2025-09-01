1441cbcf23ed4cf45d21433a080e6a1d03ea0a5d
Amal Raj <amalrajkvdev@gmail.com>
2025-09-01 16:18:32 +0530

    homescreen ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,25 @@
-import { Text } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function App() {
+  const router = useRouter();
+
   return (
-    <Text>Hello, World!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-12 mt-20 mb-5 mx-auto" />
+        <View className="flex-1 mt-5">
+          <SearchBar onPress={() => router.push("/search")} placeholder="Search" />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
